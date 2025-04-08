b314bdd9868598ed845170a03987b6baa1f2e8ac
binkcoding <malachysutton@gmail.com>
2025-04-08 16:49:41 +1000

    Added while loop for repeating calculations

--- basic-calculator.py
+++ basic-calculator.py
@@ -20,19 +20,22 @@ operations = {
     "subtract": lambda first_number, second_number: first_number - second_number,
     "divide": lambda first_number, second_number: first_number / second_number if second_number != 0 else "Cannot divide by zero",
 }
-#Input
-equation = input("What's the type of equation you'd like to do? ").lower()
-firstnumber = float(input("What's the first number? "))
-secondnumber = float(input("What's the second number? "))
 
-if equation in operation_aliases:
-    equation = operation_aliases[equation]
+while True:
+    equation = input("What's the type of equation you'd like to do? Or type 'exit' to quit. ").lower()
+    if equation == "exit":
+        break
+    firstnumber = float(input("What's the first number? "))
+    secondnumber = float(input("What's the second number? "))
+
+    if equation in operation_aliases:
+        equation = operation_aliases[equation]
 
 #Making the result presented a whole number if it is
-if equation in operations:
-    result = operations[equation](firstnumber, secondnumber) #Calling lambda
+    if equation in operations:
+        result = operations[equation](firstnumber, secondnumber) #Calling lambda
 
-if isinstance(result, float) and result.is_integer():
-    print(f"{int(result)}") #If the result is an integer
-else:
-    print(f"{result:.2f}") #If result is float
+    if isinstance(result, float) and result.is_integer():
+        print(f"{int(result)}") #If the result is an integer
+    else:
+        print(f"{result:.2f}") #If result is float
